bcb08c65dbf2cf1ce39d4e54ec3e5cf82e5b6953
Patrick Motta <patrick.ramos.motta@gmail.com>
2020-06-30 20:08:48 -0300

    002

--- src/App.js
+++ src/App.js
@@ -1,59 +1,82 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 import api from './services/api';
 
-import "./styles.css";
+import './styles.css';
 
 function App() {
-  const [ repositories, setRepositories ] = useState([]);
+  const [repos, setRepos] = useState([]);
+  const [title, setTitle] = useState('');
 
   useEffect(() => {
-     api.get('/repositories').then(response => {
-      setRepositories(response.data);
-     });
-  }, [handleRemoveRepository()]);
+    async function fetchRepos() {
+      try {
+        const { data } = await api.get('/repositories');
+
+        setRepos(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchRepos();
+  }, []);
 
   async function handleAddRepository() {
-    
-    const response = await api.post('/repositories',{
-      title: `novo projeto ${Date.now()}`,
-   });
+    try {
+      const repo = { title };
+      const { data } = await api.post('/repositories', repo);
 
-   const repositorie = response.data;
-
-   setRepositories([...repositories, repositorie]);
+      setRepos((prevRepos) => [...prevRepos, data]);
+      setTitle('');
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
-    //const response = 
-    await api.delete(`/repositories/${id}`);
+    try {
+      await api.delete(`/repositories/${id}`);
 
-    //const repositorie = response.data;
+      const updatedRepos = repos.filter((repo) => repo.id !== id);
+      setRepos(updatedRepos);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-   //setRepositories([...repositories, repositorie]);
-   //setRepositories([...repositories]);
+  function handleTitleChange(e) {
+    setTitle(e.target.value);
   }
 
   return (
-    <div>
+    <div className="container">
+      <div className="input-container">
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={title}
+          onChange={handleTitleChange}
+        />
+        <button className="btn-add" onClick={handleAddRepository}>
+          Adicionar
+        </button>
+      </div>
+
       <ul data-testid="repository-list">
-
-          {repositories.map(repositorie => 
-
-            <li key={repositorie.id}>{repositorie.title} 
-
-              <button onClick={() => handleRemoveRepository(repositorie.id)}>
-                Remover
-              </button>
-            
-            </li>
-
-          )}
- 
+        {repos.map((repo) => (
+          <li key={repo.id}>
+            {repo.title}
+            <button
+              className="btn-remove"
+              onClick={() => handleRemoveRepository(repo.id)}
+            >
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
